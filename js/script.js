6a1e779fe702328467c249a66b845723c1459e4c
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-07-07 14:42:11 +0200

    snack 2

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ Esegui questo programma in due versioni, con il for e con il while.
 3 - stampa della somma - console.
 
 */
-
+console.log('Snack 1');
 //DISPLAY
 
 var resultFor = document.getElementById('resultfor');
@@ -28,7 +28,7 @@ var number;
 for (i = 1; i <= 5; i++) {
     var number = parseInt(prompt('Inserisci un numero ' + i + '/5'));
 
-    //validation
+    //validation 
 
     if (!isNaN(number)) {
         sum += number;
@@ -63,3 +63,46 @@ while (i <= 5) {
 //output
 console.log('Totale: ', sumWhile);
 resultWhile.innerText = 'Il totale del While :' + sumWhile;
+
+
+
+
+
+
+/********************************** Snack 2 ********************************************/
+
+
+/* SNACK 2: Chiedi un numero all'utente, se è pari stampa il numero,
+se è dispari stampa il numero successivo.
+
+1- richiesta numero all'utente via prompt e store in var (ovvio).
+2 - ciclo if che stabilisce se numero pari o dispari (%2===0 pari).
+2a- else che aggiunge +1 al numero digitato.
+3- stampa in console */
+
+
+console.log('Snack 2');
+alert('Snack 2');
+
+var oddEven = document.getElementById('paridispari');
+
+var userNumber = parseInt(prompt('Inserisci un numero'));
+
+//validation
+while (isNaN(userNumber)) {
+    userNumber = parseInt(prompt('Inserisci un numero'));
+}
+
+// condizioni
+if (userNumber % 2 === 0) {
+    console.log('Numero pari :', userNumber);
+    oddEven.innerText = 'Numero pari :' + userNumber;
+} else {
+    console.log('Numero dispari +1: ', userNumber + 1);
+    oddEven.innerText = 'Numero dispari +1: ' + (userNumber + 1);
+}
+
+
+
+
+
